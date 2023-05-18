7e9a6779accf178e7fcc52eec4efe339bf5d91ec
Sander van den Hoogen <svdhoogen1999@gmail.com>
2023-05-18 16:46:37 +0200

    Initial commit

---
+++ src/math.js
@@ -0,0 +1,13 @@
+// Correctly sums a and b
+const sum = (a, b) => a + b;
+
+// Correctly subtracts a and b
+const subtract = (a, b) => a - b;
+
+// Correctly multiplies a and b
+const multiply = (a, b) => a * b;
+
+// Wrongly divides a and b
+const divide = (a, b) => b / a;
+
+module.exports = {sum, subtract, multiply, divide}
---
+++ tests/math.test.js
@@ -0,0 +1,25 @@
+const math = require('./../src/math');
+
+test('adding 1 + 2 should equal 3', () => {
+  const sumResult = math.sum(1, 2);
+
+  expect(sumResult).toBe(3);
+});
+
+test('subtracting 2 - 1 should equal 1', () => {
+  const subtractResult = math.subtract(2, 1);
+
+  expect(subtractResult).toBe(1);
+});
+
+test('multiplying 2 x 2 should equal 4', () => {
+  const multiplyResult = math.multiply(2, 2);
+
+  expect(multiplyResult).toBe(4);
+});
+
+test('dividing 4 / 2 should equal 2', () => {
+  const divideResult = math.multiply(4, 2);
+
+  expect(divideResult).toBe(2);
+});
